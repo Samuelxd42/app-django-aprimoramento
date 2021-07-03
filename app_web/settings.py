6b9d1fa8f7c957b0c81c7418d534128eb91ec80d
Samuel Fernandes <samueldiablo73@gmail.com>
2021-07-03 13:04:15 -0300

    add collection

--- app_web/settings.py
+++ app_web/settings.py
@@ -136,7 +136,7 @@ STATIC_URL = '/static/'
 
 LOGIN_URL = '/login/'
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+DISABLE_COLLECTSTATIC=1
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
